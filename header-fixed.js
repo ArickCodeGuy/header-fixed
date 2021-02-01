3de1d4d09065728446ee38a390fa961d7c676b28
unknown <arickmail1997@gmail.com>
2021-02-01 14:18:16 +0300

    first commit

---
+++ header-fixed.js
@@ -0,0 +1,18 @@
+// .header position-fixed
+// add spacing to top
+// headerFake is fake header with top padding to prevent header overlapping body on top
+let headerElem = document.querySelector('header.header');
+let headerHeight = 0;
+let headerFake = document.createElement('div');
+headerFake.classList.add('header-fake');
+headerElem.after(headerFake);
+
+headerElem.classList.add('position-fixed');
+headerResize = function () {
+  if (headerHeight != headerElem.clientHeight) {
+    headerFake.style.paddingTop = `${headerElem.clientHeight}px`;
+  };
+};
+headerResize();
+
+window.addEventListener('resize', headerResize, true);
