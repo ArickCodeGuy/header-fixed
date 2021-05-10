94982430d6b3a6bb26bc9119334bfeb9415ae95c
ArickCodeGuy <arickmail1997@gmail.com>
2021-05-10 16:04:12 +0300

    defaultOptions.topOffset now can be specified as height of HTMLElement. defaultOptions.fakeDivClass support added. defaultOptions.init added. little code refactoring. And some small stuff

--- main.js
+++ main.js
@@ -1,62 +1,61 @@
 'use strict';
 
-let defaults = {
-  topOffset: 0,
-  class: ''
+let defaultOptions = {
+  init: true, /*init or not init instantly*/
+  topOffset: 0, /*value in pixels or height of specified html element*/
+  fakeDivClass: '', /*add custom class to fake div (fake-div class still will be added)*/
 }
 
 class StickyEl {
   constructor(el, options) {
-    this.el = document.querySelectorAll(el);
-    this.options = {...defaults};
-    this.setOptions(options);
-    this.init(this.el);
-  }
-
-  setOptions(options) {
-    if (options) {
-      for (let option in options) {
-        this.options[option] = options[option];
-      };
-      return 'options set to your options obj'
-    }else {
-      return 'options set to default options'
-    }
+    this.el = el;
+    this.options = Object.assign({}, defaultOptions, options);
+    this.options.init === true ? this.init(): false;
   }
 
   isScrolled(el) {
-    let elFake = this.getFake(el);
-    if (el.dataset.stickyOffsetTop < window.scrollY) {
+    const elFake = this.getFake(el);
+    let topOffset;
+    if (typeof this.options.topOffset === 'number') {
+      topOffset = this.options.topOffset;
+    }else if (HTMLElement.prototype.isPrototypeOf(this.options.topOffset)) {
+      topOffset = this.options.topOffset.clientHeight;
+    };
+    this.iDontKnowHowToNameThisFunction(el);
+    const fixedPos = el.dataset.stickyOffsetTop - topOffset;
+    if (fixedPos  < window.scrollY) {
       el.classList.add('scrolled');
       elFake.style.display = 'block';
       el.style.left = `${el.offsetLeft}px`;
-      el.style.top = `${this.options.topOffset}px`;
+      el.style.top = `${topOffset}px`;
       el.style.position = 'fixed';
     }else {
       el.classList.remove('scrolled');
       elFake ? elFake.style.display = 'none':false;
-
       el.style.position = '';
     };
   }
 
   createFake(el) {
-    let elFake = document.createElement('div');
+    const elFake = document.createElement('div');
     elFake.classList.add('fake-div');
-    this.options.class && typeof this.options.class === 'string' ? elFake.classList.add(this.options.class): false;
+    this.options.fakeDivClass && typeof this.options.fakeDivClass === 'string' ? elFake.classList.add(this.options.fakeDivClass): false;
     elFake.style.display = 'none';
     elFake.style.height = `${el.clientHeight}px`;
     el.after(elFake);
   }
 
-  addAttrs(el) {
-    let elOffset = el.offsetTop - this.options.topOffset
-    el.dataset.stickyOffsetTop = elOffset;
-    console.log(elOffset);
+  iDontKnowHowToNameThisFunction(el) {
+    if (el.style.position != 'fixed') {
+      el.dataset.stickyOffsetTop = el.offsetTop;
+    }else {
+      const fakeDiv = el.nextElementSibling;
+      el.dataset.stickyOffsetTop = fakeDiv.offsetTop;
+    }
   }
 
   getFake(el) {
-    let elFake = el.nextElementSibling;
+    const elFake = el.nextElementSibling;
     if (elFake.classList.contains('fake-div')) {
       return elFake
     }else {
@@ -65,7 +64,7 @@ class StickyEl {
   }
 
   windowResize(el) {
-    let elFake = this.getFake(el);
+    const elFake = this.getFake(el);
     elFake ? elFake.style.height = `${el.clientHeight}px`: false;
   }
 
@@ -74,12 +73,21 @@ class StickyEl {
     window.addEventListener('scroll', () => this.isScrolled(el));
   }
 
-  init(elArr) {
-    elArr.forEach((el) => {
-      this.createFake(el);
-      this.addAttrs(el);
-      this.addEvents(el);
-    });
+  initSingleElement(el) {
+    this.createFake(el);
+    this.iDontKnowHowToNameThisFunction(el);
+    this.addEvents(el);
   }
 
+  initArray(arr) {
+    arr.forEach((el) => this.initSingleElement(el));
+  }
+
+  init() {
+    // if string is specified
+    if (typeof this.el === 'string') {
+      const el = document.querySelectorAll(this.el);
+      el ? this.initArray(el): false;
+    };
+  }
 };
